d59b63e163e25479b1f6d36f5d99d1dd209921e5
danwaters <dwaters@gmail.com>
2014-08-26 14:49:30 -0700

    Initial commit.

---
+++ ShellTools.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace XamStore.Android.Tests
+{
+	public class ShellTools
+	{
+		private const string WifiPackage = "Xamarin.AutomationTools";
+
+		public ShellTools ()
+		{
+
+		}
+
+		public void DisableWifi()
+		{
+			var info = new ProcessStartInfo ("~/Library/Developer/Xamarin/android-sdk-mac_x86/platform-tools/adb");
+			var process = Process.Start (info);
+
+			if (process == null)
+				throw new NullReferenceException ("Null it's.");
+
+			process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => {
+				Console.Write("ERROR: " + e.Data);
+			};
+
+			process.OutputDataReceived += (object sender, DataReceivedEventArgs e) => {
+				Console.Write("DATA: " + e.Data);
+			};
+		}
+	}
+}
+
